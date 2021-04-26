d3d653ee3b21059ecdcc2b3ba5addf0203e62298
John Eskew <eskew.john@gmail.com>
2021-04-26 06:28:21 -0500

    finish off program with linux support

--- ConcThreads.go
+++ ConcThreads.go
@@ -11,16 +11,17 @@ import(
   "strconv"
   "log"
   "syscall"
+  "strings"
 )
 
-const timeDelay=10;
+//These 4 constants are used to the determine the complexity of the GoRoutine. 
+const timeDelay=1;
 const randIterations=3;
 const blockExecution=false;
-const fixedSeed=false;
+const fixedSeed=true;
 
 //TODO: Make functions for:
-// 2. populateSlice
-// 3. goRoutineWorker
+// goRoutineWorker
 
 //TODO: Make it work on Linux
 //TODO: Get better grasp on GoRoutine sync calls, defer, & sleep
@@ -32,9 +33,9 @@ func main(){
   //The following block creates a slice of numbers from 2^0 up to 2^17
   binarySlice := make([]int, 1)
   binarySlice[0] = 1
-  for i:= 0; binarySlice[i] < 100000; i++{
+  for i:= 0; binarySlice[i] < 1000000; i++{
     binarySlice = append(binarySlice, binarySlice[i]*2)
-    if binarySlice[i] > 100000{
+    if binarySlice[i] > 1000000{
       break
     }
   }
@@ -44,18 +45,26 @@ func main(){
   additionalThreads := make([]int, len(binarySlice))
   runTime := make([]time.Duration, len(binarySlice))
   
-  origThreadCount := getThreadCountWindows()
+  origThreadCount := 0
+  if runtime.GOOS == "windows"{
+    origThreadCount = getThreadCountWindows()
+  } else if runtime.GOOS == "linux"{
+    origThreadCount = getThreadCountLinux()
+  }
+  
   fmt.Printf("Thread count before work: %d\n", origThreadCount)
+
+  timeStartTotal := time.Now()
 
   //For each value from 2^0 to 2^n
   for i := 0; i < len(binarySlice); i++{
-    var concurrentGoRoutines = 0
-    var maxConcurrentGoRoutines = 0
+    concurrentGoRoutines := 0
+    maxConcurrentGoRoutines := 0
     
-    var concThreads = 0
-    var maxConcThreads = 0
+    concThreads := 0
+    maxConcThreads := 0
     
-    var timeStart = time.Now();
+    timeStart := time.Now()
 
     var wg sync.WaitGroup
 
@@ -68,6 +77,10 @@ func main(){
       //In Go-Routine: Generating several random 64 bit floats with various delays
       go func(){
         concurrentGoRoutines++
+        if(blockExecution){
+          defer wg.Done() //"On return, notify the WaitGroup that we're done" 
+        }
+        
         for h := 0; h < randIterations; h++{
           var r *rand.Rand
           if(fixedSeed){
@@ -85,10 +98,18 @@ func main(){
         maxConcurrentGoRoutines = concurrentGoRoutines
       }
     }
+    if(blockExecution){
+      wg.Wait()
+    }
 
-    //I wanted to include the follow block inside of the GoRoutine, but this command call(on Windows at least) is too expensive and locks up the program. Ultimately I don't believe this matters because I found that the threads take quite a bit of time to be cleaned up (on the order of seconds).
+    //I wanted to include the following block inside of the GoRoutine, but this command call(on Windows at least) is too expensive and locks up the program. Ultimately I don't believe this matters because I found that the threads take quite a bit of time to be cleaned up (on the order of seconds).
     if runtime.GOOS == "windows" {
       concThreads = getThreadCountWindows()
+      if concThreads > maxConcThreads{
+        maxConcThreads = concThreads
+      }
+    }else if runtime.GOOS == "linux" {
+      concThreads = getThreadCountLinux()
       if concThreads > maxConcThreads{
         maxConcThreads = concThreads
       }
@@ -101,27 +122,48 @@ func main(){
     additionalThreads[i] = maxConcThreads - origThreadCount
     runTime[i] = timeTotal
   }
+  
+  var timeEndTotal = time.Now()
+  var timeTotalTotal = timeEndTotal.Sub(timeStartTotal)
 
-  //Print the results
+  fmt.Print("Total Run Time: ", timeTotalTotal, "\n")
   printResults(binarySlice, concurrentRoutines, concurrentThreads, additionalThreads, runTime)
 }
 
 func printSysAndRTInfo(){
-  fmt.Printf("Go version: %s\n", runtime.Version())
-  fmt.Printf("OS: %s\n", runtime.GOOS)
-  fmt.Print("Blocking calls: ")
+  fmt.Println("System info:")
+  fmt.Printf("\tGo version: %s\n", runtime.Version())
+  fmt.Printf("\tOS: %s\n", runtime.GOOS)
+  fmt.Println()
+
+  fmt.Println("Options:")
+  fmt.Printf("\tTime Delay: %d seconds\n", timeDelay)
+  fmt.Printf("\tRandIterations: %d\n", randIterations)
+  fmt.Print("\tBlocking calls: ")
   if(blockExecution){
     fmt.Print("true\n")
   } else{
     fmt.Print("false\n")
   }
-  fmt.Print("Fixed Seed: ")
+  fmt.Print("\tFixed Seed: ")
   if(fixedSeed){
     fmt.Print("true\n")
   } else{
     fmt.Print("false\n")
   }
   fmt.Println()
+}
+
+func getThreadCountLinux() int{
+  var concThreads = 0
+  if runtime.GOOS == "linux"{
+    stdOut, err := exec.Command("ps", "huH", "p", strconv.Itoa(os.Getpid())).Output()
+    if err != nil {
+      fmt.Sprintf("Failed to execute command: %s", err)
+    }
+    concThreads = len(strings.Split(string(stdOut), "\n")) - 1
+  }
+  return concThreads;
 }
 
 func getThreadCountWindows() int{
@@ -145,7 +187,7 @@ func getThreadCountWindows() int{
 }
 
 func printResults(binarySlice []int, concurrentRoutines []int, concurrentThreads []int, additionalThreads []int, runTime []time.Duration){
-  fmt.Println("\tTot. GR\t\tMax GR\t\tMax Threads\tAddl. Threads\tRun Time")
+  fmt.Println("\tTot. GR\t\tMax Conc. GR\tMax Threads\tAddl. Threads\tRun Time")
   for i:= 0; i < len(binarySlice); i++{
     fmt.Printf("\t%d: ", binarySlice[i])  
     if(binarySlice[i] < 100000){
@@ -154,8 +196,6 @@ func printResults(binarySlice []int, concurrentRoutines []int, concurrentThreads
     fmt.Printf("\t%d", concurrentRoutines[i])
     fmt.Printf("\t\t%d", concurrentThreads[i])
     fmt.Printf("\t\t%d", additionalThreads[i])
-    fmt.Print("\t\t")
-    fmt.Print(runTime[i])
-    fmt.Print("\n")
+    fmt.Print("\t\t", runTime[i], "\n")
   }
 }
